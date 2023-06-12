394c4cc95e9c5c843fcbc6d3bb6d3ce86593fcae
Jake Horner <jake.horner@ergogroup.ie>
2023-06-12 19:23:36 +0100

    application is now live on ec2 instance
    git ignore added to not expose db/ port numbers

--- PersonalPortfolio/src/main/java/com/jake/PersonalPortfolioApplication.java
+++ PersonalPortfolio/src/main/java/com/jake/PersonalPortfolioApplication.java
@@ -2,9 +2,12 @@ package com.jake;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
